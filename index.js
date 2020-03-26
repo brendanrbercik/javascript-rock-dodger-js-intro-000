00bfc799dc55cb19bce47f3d0803f901d36ba98e
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 23:24:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,20 +35,12 @@ function checkCollision(rock) {
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = rockLeftEdge+20;
 
-    if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge)
-    {
-      return true
-    }
-    else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge)
-    {
-      return true
-    }
-    else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge >= dodgerRightEdge)
-    {
-      return true
-    }
+    return (
+      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
+    )
   }
-  return false;
 }
 
 function createRock(x) {
