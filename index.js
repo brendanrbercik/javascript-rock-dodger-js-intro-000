c6b3b4b4bcd6602f330ed93c6fe687d21cb5a69e
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 21:19:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -139,14 +139,15 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if (e.which == LEFT_ARROW) {
-     moveDodgerLeft();
-     preventDefault();
-     stopPropagation();
+     e.moveDodgerLeft();
+     e.preventDefault();
+     e.stopPropagation();
    }
+
    if (e.which == RIGHT_ARROW) {
-     moveDodgerRight();
-     preventDefault();
-     stopPropagation();
+     e.moveDodgerRight();
+     e.preventDefault();
+     e.stopPropagation();
    }
 }
 
