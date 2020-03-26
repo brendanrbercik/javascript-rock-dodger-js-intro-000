2bd1e85a9c3c700b94e0d55a13c8b74630e70800
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 22:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function createRock(x) {
         * Otherwise, if the rock hasn't reached the bottom of
         * the GAME, we want to move it again.
         */
-        
+
         else if (top < 360) {
          window.requestAnimationFrame(step);
        }
