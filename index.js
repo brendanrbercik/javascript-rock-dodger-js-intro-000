c8e3ab3fb5755f824b0363eec9380984d4b1a29b
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 22:18:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -103,6 +103,9 @@ function createRock(x) {
         * But if the rock *has* reached the bottom of the GAME,
         * we should remove the rock from the DOM.
         */
+        else {
+          ROCKS.shift();
+        }
      }
 
 
