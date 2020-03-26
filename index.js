3a6511234f7343ebdc26ab0793ca4b7ab7906c27
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 21:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -140,13 +140,13 @@ function moveDodger(e) {
    */
    if (e.which == LEFT_ARROW) {
      moveDodgerLeft();
-     e.preventDefault;
-     e.stopPropagation;
+     preventDefault();
+     stopPropagation();
    }
    if (e.which == RIGHT_ARROW) {
      moveDodgerRight();
-     e.preventDefault;
-     e.stopPropagation;
+     preventDefault();
+     stopPropagation();
    }
 }
 
