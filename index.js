3e183f5899116d5b156d522e1be9680430317f13
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 23:14:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function createRock(x) {
 }
 
   // We should kick off the animation of the rock around here.
-  window.requestAnimationFrame(step);
+  window.requestAnimationFrame(moveRock);
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision.
